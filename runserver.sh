ecc0b9780e8ece91ad7fcbdb6ed51a0ee565ecfb
Ryan Haas <haasrr@outlook.com>
2025-03-02 20:57:20 -0500

    Updated runserver script

--- runserver.sh
+++ runserver.sh
@@ -1,1 +1,12 @@
-python manage.py runserver --settings=etsu_office_hours.settings_dev
+#! /bin/bash -
+
+if [ -f ".venv/bin/activate" ]
+  then source .venv/bin/activate; echo "Activated venv"
+elif [ -f ".venv/Scripts/activate" ]
+  then source .venv/Scripts/activate; echo "Activated venv"
+else
+  echo "Unable to activate venv."
+  echo "Please create a Python virtual environment named `.venv` in this directory"
+fi
+
+python manage.py runserver 0.0.0.0:8000 --settings=etsu_office_hours.settings_dev
